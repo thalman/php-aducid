4eb6ccdfcf3561407c12233345e48743b9a3df5e
Tomas Halman <tomas@halman.net>
2015-03-08 19:34:18 +0100

    use transaction verify for local factor operation

--- tests/local_factor_operations.php
+++ tests/local_factor_operations.php
@@ -2,25 +2,25 @@
 
 function action_initLF(){
     $aducid = new AducidSessionClient($GLOBALS['aim']);
-    $aducid->initLocalFactor($aducid->currentURL() . "?action=verify");
+    $aducid->initLocalFactor($aducid->currentURL() . "?action=verifytransaction");
     $aducid->invokePeig();
 }
 
 function action_changeLF() {
     $aducid = new AducidSessionClient($GLOBALS['aim']);
-    $aducid->changeLocalFactor($aducid->currentURL() . "?action=verify");
+    $aducid->changeLocalFactor($aducid->currentURL() . "?action=verifytransaction");
     $aducid->invokePeig();
 }
 
 function action_deleteLF() {
     $aducid = new AducidSessionClient($GLOBALS['aim']);
-    $aducid->deleteLocalFactor($aducid->currentURL() . "?action=verify");
+    $aducid->deleteLocalFactor($aducid->currentURL() . "?action=verifytransaction");
     $aducid->invokePeig();
 }
 
 function action_verifyLF() {
     $aducid = new AducidSessionClient($GLOBALS['aim']);
-    $aducid->verifyLocalFactor($aducid->currentURL() . "?action=verify");
+    $aducid->verifyLocalFactor($aducid->currentURL() . "?action=verifytransaction");
     $aducid->invokePeig();
 }
 
